8ea418e6a884fe29f57de690912bf6efd34b12b3
Emmanuel Rudelle <erudelle@meetphone.com>
2016-10-21 09:19:38 +0200

    Add auth reducer

--- public/src/reducers/index.js
+++ public/src/reducers/index.js
@@ -2,16 +2,9 @@ import { combineReducers } from 'redux'
 import alerts from './alerts';
 import auth from './authentication';
 
-
-
-
-
-
-
-
 export const rootReducer = combineReducers({
   alerts
-  
+  auth
 })
 
 
